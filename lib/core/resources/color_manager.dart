9553595b67bbc083ea4ad2a9e864b0ac76f9bf76
abdel <z.elfouly.z@gmail.com>
2026-04-16 12:55:37 +0200

    core setup

---
+++ lib/core/resources/color_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFF014162);
+  static const Color white = Color(0xFFF5F7F7);
+  static const Color appbarBackground = Color(0xFFF7FCFF);
+  static const Color black = Color(0xFF000016);
+  static const Color grey = Color(0xFFDAD8D8);
+  static const Color error = Color(0xffDB6E6E);
+}
