a486d02573a888d3e6c87b7999e4ed9dc98decb4
Mahmoud_Hassanein <mahmoudmohamedhassanein5@gmail.com>
2026-04-23 21:36:54 +0200

    refactor: clean up code by removing unnecessary lines and updating pubspec.yaml formatting

--- lib/core/failure/failure.dart
+++ lib/core/failure/failure.dart
@@ -1,5 +1,4 @@
 class Failure {
   final String failuremessage;
   Failure([this.failuremessage = 'Something Went wrong']);
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 
 import 'SplashView.dart';
@@ -15,11 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:ForgotPasswordView(
-
-      ) ,
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashView());
   }
 }
